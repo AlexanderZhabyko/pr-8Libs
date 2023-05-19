7b0607a460aed5b25ad2711e882db640918e85a0
AlexanderZhabyko <isip_a.a.zhabyko@mpt.ru>
2023-05-19 22:29:37 +0300

    Libs

---
+++ DerSerLib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace DerSerLib
+{
+    public class jsonclass
+    {
+        private static string rootFolder = Path.GetFullPath(@"..\..\..\");
+
+        public static void JsonSer<T>(T type, string fileName)
+        {
+            string JsonWrite = JsonConvert.SerializeObject(type);
+            File.WriteAllText(rootFolder + "\\" + fileName, JsonWrite);
+        }
+        public static T JsonDeser<T>(string fileName)
+        {
+            string JsonRead = File.ReadAllText(rootFolder + "\\" + fileName);
+            T Mytype = JsonConvert.DeserializeObject<T>(JsonRead);
+            return Mytype;
+        }
+    }
+}
---
+++ PR8Libs/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DerSerLib;
+
+
+namespace PR8Libs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string name = "test.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void serbtn_Click(object sender, RoutedEventArgs e)
+        {
+            jsonclass.JsonSer(textblock1.Text, name);
+            textblock1.Text = "Записано в " + name;
+        }
+
+        private void deserbtn_Click(object sender, RoutedEventArgs e)
+        {
+            string json = jsonclass.JsonDeser<string>(name);
+            textblock1.Text = json;
+        }
+    }
+}
